40cd78c2c43691fbb25b5337f6c03ac034e5f3c5
Pablo Salut <pablosalut03@gmail.com>
2023-08-28 17:55:24 -0300

    Accept tests aiming prod

--- cypress/e2e/play_game.cy.js
+++ cypress/e2e/play_game.cy.js
@@ -1,7 +1,7 @@
 describe('Main menu', () => {
   it('main menu must work correctly', () => {
     cy.viewport(1920, 1080) // Set viewport to 550px x 750px
-    cy.visit('http://localhost:8080/', {timeout: 500000});
+    cy.visit('https://ahorcado-webpack.vercel.app/', {timeout: 500000});
     cy.contains('AHORCADO - GROUP 4');
 
     cy.get("#playButton").click();
