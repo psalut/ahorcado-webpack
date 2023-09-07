139c2061abcdd020dc1c45802e0238bb0bbfee8f
Pablo Salut <pablosalut03@gmail.com>
2023-09-06 23:04:59 -0300

    Config eslint to ignore node_modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  ignorePatterns: ['node_modules/'],
+  ignorePatterns: ['node_modules/', 'dist', 'newdist'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
